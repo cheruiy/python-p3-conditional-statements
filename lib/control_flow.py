8d2ff64e93d6fdaf43c36e254cdb8570a96aa4c0
Nyole <you@example.com>
2024-05-23 00:17:11 +0300

    complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,46 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username.lower() == "admin" or username.upper() == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+    # pass
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif 40 <= temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
+    # pass
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    # pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    operations = {
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        '/': num1 / num2
+    }
+    # return operations.get(operation, "Invalid operation!")
+    result = operations.get(operation)
+    if result is None:
+        print("Invalid operation!")
+        return None
+    return result
+    # pass
